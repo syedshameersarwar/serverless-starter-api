a8a6102fece2e1c0b62ba915ecca4f5685141d16
syedshameersarwar <syedshameersarwar@gmail.com>
2019-12-22 00:02:15 +0500

    Finished App

--- get.js
+++ get.js
@@ -3,7 +3,7 @@ import { success, failure } from "./libs/response-lib";
 
 export const main = async (event, context) => {
   const params = {
-    TableName: process.env.TableName,
+    TableName: process.env.tableName,
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
       noteId: event.pathParameters.id
